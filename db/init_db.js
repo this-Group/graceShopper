5de7de6d6e9d8df8604efd1db809fa51f569b7d1
Alan Sioco <sioco@alans-mbp.lan>
2022-01-30 16:07:29 -0500

    first commit

--- db/init_db.js
+++ db/init_db.js
@@ -4,22 +4,59 @@ const {
   // other db methods 
 } = require('./index');
 
+async function dropTables() {
+  console.log('dropping all tables ...');
+  try {
+    await client.query(`
+    
+    DROP TABLE IF EXISTS products;
+    `)
+    
+  } catch (error) {
+    console.error(error)
+    
+  }
+}
+
 async function buildTables() {
   try {
-    client.connect();
-
+    await client.connect(`
+    CREATE TABLE products(
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(255) UNIQUE NOT NULL,
+      description VARCHAR(255) NOT NULL,
+      price DECIMAL,
+      qty INTEGER
+    );
+`);
+ console.log('finished building product table')
     // drop tables in correct order
 
     // build tables in correct order
 
   } catch (error) {
+    console.error("error with products table")
     throw error;
   }
-}
+};
 
 async function populateInitialData() {
   try {
     // create useful starting data
+    const albumsToCreate = [
+      {title:'', description: '', price: '', qty: ''},
+      {title:'', description: '', price: '', qty: ''},
+      {title:'', description: '', price: '', qty: ''},
+      {title:'', description: '', price: '', qty: ''},
+      {title:'', description: '', price: '', qty: ''},
+      {title:'', description: '', price: '', qty: ''},
+      {title:'', description: '', price: '', qty: ''},
+      {title:'', description: '', price: '', qty: ''},
+      {title:'', description: '', price: '', qty: ''},
+      {title:'', description: '', price: '', qty: ''},
+      {title:'', description: '', price: '', qty: ''},
+      
+    ]
   } catch (error) {
     throw error;
   }
