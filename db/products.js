9a21a68652f2180968483e84c825999a83dc538d
Mr_Jerry <jerrykano@gmail.com>
2022-01-30 21:13:34 -0800

    API Routes added for getAllProducts() , getProductById. Added DB functions.

---
+++ db/products.js
@@ -0,0 +1,39 @@
+const { client } = require("./index");
+
+async function getAllProducts() {
+    try {
+      const { rows: products } = await client.query(
+        `
+        SELECT *
+        FROM products;
+        `
+      );
+      return products;
+    } catch (error) {
+      throw error;
+    }
+  }
+  
+  async function getProductById(productId) {
+    try {
+      const {
+        rows: [product],
+      } = await client.query(
+        `
+        SELECT *
+        FROM products
+        WHERE id = $1;
+        `,
+        [productId]
+      );
+      return product;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  module.exports = {
+    getAllProducts,
+    getProductById
+
+  };
