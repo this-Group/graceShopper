4840d4ebe2189e5ed2ae496f3f652b8e65ce07cb
LukeLowrey2 <89051599+LukeLowrey2@users.noreply.github.com>
2022-02-03 20:48:46 -0800

    added api routes and methods for users

---
+++ db/users.js
@@ -0,0 +1,64 @@
+const { client } = require("./index");
+
+async function createUser( username, password ) {
+    try {
+        const { rows: [user] } = await client.query(`
+            INSERT INTO users(username, password)
+            VALUES ($1, $2)
+            ON CONFLICT (username) DO NOTHING;
+            `, [username, password]);
+
+            return user
+
+    } catch (error) {
+        console.log('createUser function failed');
+        console.error(error);
+        throw error;
+    }
+}
+
+async function loginUser( username, password ) {
+    try {
+        const { rows: [user] } = await client.query(`
+            SELECT id 
+            FROM users
+            WHERE username = $1, password = $2)
+            LIMIT 1;
+        `, [username, password]);
+
+        return user;
+
+    } catch (error) {
+        console.log('getUser function failed');
+        console.error(error);
+        throw error;
+    }
+}
+
+async function checkForUsername(username) {
+    try {
+        const { rows: [user] } = await client.query(`
+            SELECT usename 
+            FROM users
+            WHERE username = $1);
+        `, [username]);
+
+        if(user){
+            return true;
+        }else {
+            return false
+        }
+        
+
+    } catch (error) {
+        console.log('getUserByUsername function failed');
+        console.error(error);
+        throw error;
+    }
+}
+
+module.exports = {
+    createUser,
+    loginUser,
+    checkForUsername
+  };
