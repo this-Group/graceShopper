3199520dc8f1007346329e9bf83022dae5d5b1de
Alan Sioco <sioco@Alans-MacBook-Pro.local>
2022-02-03 20:44:11 -0800

    connect database

--- db/albums.js
+++ db/albums.js
@@ -4,7 +4,7 @@ const client = require ('./client');
 const createAlbum = async ({artist, title, description, price, qty}) => {
     try {
         const { rows: [album] } = await client.query(`
-            INSERT INTO users(artist, title, description, price, qty)
+            INSERT INTO products(artist, title, description, price, qty)
             VALUES ($1, $2, $3, $4, $5)
             RETURNING *;
         `, [artist, title, description, price, qty]);
