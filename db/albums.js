bc8fa6e64160c0922c39ade7c6af02014fc1405b
Mr_Jerry <jerrykano@gmail.com>
2022-02-05 12:47:31 -0800

    Minor changes

--- db/albums.js
+++ db/albums.js
@@ -1,16 +1,16 @@
 const { client } = require ('./client');
 
 
-async function createAlbum ({artist, title, description, price, qty}) {
+async function createAlbum ({artist, title, genre, price, qty}) {
     console.log('this is the createAlbum func')
     try {
         const { rows: [album] } = await client.query(
             `
-            INSERT INTO products(artist, title, description, price, qty)
+            INSERT INTO products(artist, title, genre, price, qty)
             VALUES ($1, $2, $3, $4, $5)
             RETURNING *;
             `,
-            [artist, title, description, price, qty]);
+            [artist, title, genre, price, qty]);
         return album;
         
     } catch (error) {
