88de7024071d709d45989ae74c43c487f28393e9
Alan Sioco <sioco@Alans-MacBook-Pro.local>
2022-02-12 19:24:30 -0800

    pic column, user rtr

--- db/albums.js
+++ db/albums.js
@@ -1,16 +1,16 @@
 const { client } = require ('./client');
 
 
-async function createAlbum ({artist, title, genre, price, qty, imageurl}) {
+async function createAlbum ({artist, title, genre, price, qty, picture}) {
     console.log('this is the createAlbum func')
     try {
         const { rows: [album] } = await client.query(
             `
-            INSERT INTO products(artist, title, genre, price, qty, imageurl)
+            INSERT INTO products(artist, title, genre, price, qty, picture)
             VALUES ($1, $2, $3, $4, $5, $6)
             RETURNING *;
             `,
-            [artist, title, genre, price, qty, imageurl]);
+            [artist, title, genre, price, qty, picture]);
         return album;
         
     } catch (error) {
---
+++ src/api/orders.js
@@ -0,0 +1,14 @@
+// const express = require('express');
+// const ordersRouter = express.Router();
+
+// ordersRouter.get('/orders', async (req, res, next) => {
+//     try {
+        
+        
+//         res.status(200.send )
+//     } catch (error) {
+//         console.error(error);
+//     }
+// });
+
+// module.exports = ordersRouter;
