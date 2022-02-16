3e083e52b162e989b70e143106c0bb48a45fae2c
LukeLowrey2 <89051599+LukeLowrey2@users.noreply.github.com>
2022-02-15 16:43:41 -0800

    updated login router

--- db/albums.js
+++ db/albums.js
@@ -1,16 +1,16 @@
 const { client } = require ('./client');
 
 
-async function createAlbum ({artist, title, genre, price, qty}) {
+async function createAlbum ({artist, title, genre, price, qty, picture}) {
     console.log('this is the createAlbum func')
     try {
         const { rows: [album] } = await client.query(
             `
-            INSERT INTO products(artist, title, genre, price, qty)
-            VALUES ($1, $2, $3, $4, $5)
+            INSERT INTO products(artist, title, genre, price, qty, picture)
+            VALUES ($1, $2, $3, $4, $5, $6)
             RETURNING *;
             `,
-            [artist, title, genre, price, qty]);
+            [artist, title, genre, price, qty, picture]);
         return album;
         
     } catch (error) {
--- db/users.js
+++ db/users.js
@@ -1,14 +1,16 @@
 const { client } = require("./client");
 
-async function createUser( username, password ) {
+async function createUser( {username, password} ) {
     try {
         const { rows: [user] } = await client.query(`
             INSERT INTO users(username, password)
             VALUES ($1, $2)
-            ON CONFLICT (username) DO NOTHING;
+            ON CONFLICT (username) DO NOTHING
+            RETURNING *;
             `, [username, password]);
 
             return user
+        
 
     } catch (error) {
         console.log('createUser function failed');
@@ -17,19 +19,30 @@ async function createUser( username, password ) {
     }
 }
 
-async function loginUser( username, password ) {
+//Should this be SELECT * indstead of just id so it returns the full user?
+async function loginUser( {username, password} ) {
     try {
         const { rows: [user] } = await client.query(`
-            SELECT id 
+            SELECT * 
             FROM users
-            WHERE username = $1, password = $2)
+            WHERE username = $1 
             LIMIT 1;
-        `, [username, password]);
+        `, [username]);
+        console.log("this is user from login func", user)
+        
 
-        return user;
+        if (!user) return null; 
+
+        const doesPasswordsMatch = compare(password, user.password);
+        console.log('the passwords match:', doesPasswordsMatch)
+
+        if (doesPasswordsMatch) return user ; 
+
+
+        
 
     } catch (error) {
-        console.log('getUser function failed');
+        console.log('loginUser function failed');
         console.error(error);
         throw error;
     }
@@ -38,9 +51,9 @@ async function loginUser( username, password ) {
 async function checkForUsername(username) {
     try {
         const { rows: [user] } = await client.query(`
-            SELECT usename 
+            SELECT username 
             FROM users
-            WHERE username = $1);
+            WHERE username = $1;
         `, [username]);
 
         if(user){
@@ -57,8 +70,28 @@ async function checkForUsername(username) {
     }
 }
 
+// async function getUserByUsername(username) {
+//     try {
+//         const {rows: [user] } = await client.query(`
+//             SELECT * 
+//             FROM users
+//             WHERE username = $1
+//             RETURNING *;
+//         `, [username]);
+
+//         return {
+//             "id": user.id,
+//             "username": user.username
+//         }
+//     } catch (error) {
+//         console.log('getUserById function failed');
+//         console.error(error);
+//     }
+// }
+
 module.exports = {
     createUser,
     loginUser,
-    checkForUsername
+    checkForUsername,
+    // getUserByUsername
   };
