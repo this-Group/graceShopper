936ffbfb37892a32d52d7f147fe0aa4052a92e0d
LukeLowrey2 <89051599+LukeLowrey2@users.noreply.github.com>
2022-02-23 20:55:31 -0800

    updated query

--- routes/orders.js
+++ routes/orders.js
@@ -13,7 +13,7 @@ const  {
 
 ordersRouter.get('/:userId', async (req, res, next) => {
     const userId = req.params;
-    console.log('this is userid', userId.id);
+    console.log('this is userid id', userId.id);
     try {
         
         const orders = await getOrderByUserId(userId.userId);
@@ -24,12 +24,14 @@ ordersRouter.get('/:userId', async (req, res, next) => {
     }
 })
 ordersRouter.get('/myorders/:id', async (req, res, next) => {
-    const orderId = req.params;
-    console.log("oderId from the body", orderId)
+    const id = req.params;
+    console.log('this is oderid id', id.id);
+    console.log("oderId from the body", id)
     try {
         
-        const order = getOrderByOrderID(orderId);
-        res.send(200).send(order);
+        const orderByOrderId = await getOrderByOrderID(id.id);
+        console.log("order from giet order by oderId", orderByOrderId)
+        res.send(200).send(orderByOrderId);
     } catch (error) {
         return next(error);
     }
@@ -60,14 +62,14 @@ ordersRouter.delete('/myorders/:id', async (req, res, next) => {
     }
 })
 ordersRouter.patch('/myorders', async(req, res, next) => {
-        const {orderId, status} = req.body
+        const {orderId, status, userId} = req.body
     try {
         console.log('orders patch ok')
-        const {id} = req.params
-        const {status} = req.body;
         const updatedOrder = updateOrderStatus(orderId, status);
-
-        res.send(updatedOrder)
+        const newStatus = "In Cart"
+        const newOrder = await createOrder ( userId, newStatus )
+        console.log("newOrder from order patch", newOrder)
+        res.send(newOrder)
     } catch (error) {
         return next (error)
     }
